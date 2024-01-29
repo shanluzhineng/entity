dc8810bafb9a643053c0f4c0d2fee81661fe2b1b
zcqing <jeseeqing@gmail.com>
2024-01-29 13:44:10 +0800

    add tenant with entity

---
+++ entity_with_tenant.go
@@ -0,0 +1,22 @@
+package entity
+
+type IEntityWithTenant interface {
+	SetTenantId(tenantId string)
+	GetTenantId() string
+}
+
+type EntityWithTenant struct {
+	TenantId string `json:"tenantId" bson:"tenantId"`
+}
+
+// #region IEntityWithUser Members
+
+func (t *EntityWithTenant) SetTenantId(tenantId string) {
+	t.TenantId = tenantId
+}
+
+func (p *EntityWithTenant) GetTenantId() string {
+	return p.TenantId
+}
+
+// #endregion
